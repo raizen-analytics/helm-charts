de6ccf924499d0e07d3c2ac5d0b0e3b535f9357a
Raphael Barros <raphael_bos@poli.ufrj.br>
2020-03-23 00:20:01 -0300

    Adding ssh to  code image

--- images/code-server/entrypoint.sh
+++ images/code-server/entrypoint.sh
@@ -2,4 +2,4 @@
 if [ -n "$USERNAME" ]; then
     sudo USERNAME=$USERNAME bash -c 'echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/nopasswd && usermod -l $USERNAME coder';
 fi
-dumb-init code-server --host 0.0.0.0 $@
+sudo service ssh restart && dumb-init code-server --host 0.0.0.0 $@
