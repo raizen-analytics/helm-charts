274cc11b58d67f1d7e93562b97f823bd072052a9
Raphael Barros <raphael_bos@poli.ufrj.br>
2020-04-06 15:31:04 -0300

    Fixing entrypoint

--- images/code-server/entrypoint.sh
+++ images/code-server/entrypoint.sh
@@ -2,5 +2,4 @@
 if [ -n "$USERNAME" ]; then
     sudo USERNAME=$USERNAME bash -c 'echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/nopasswd && usermod -l $USERNAME coder';
 fi
-["dumb-init", "fixuid", "-q", "/usr/local/bin/code-server", "--host", "0.0.0.0", "."]
-sudo service ssh restart && dumb-init fixuid -q /usr/local/bin/code-server --host 0.0.0.0 . $@
+sudo service ssh restart && dumb-init /usr/local/bin/code-server --host 0.0.0.0 . $@
