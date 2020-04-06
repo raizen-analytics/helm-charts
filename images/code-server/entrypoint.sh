b062f3c45c5632e782b318bd568db7fe9d3cb30b
Raphael Barros <raphael_bos@poli.ufrj.br>
2020-04-06 13:10:01 -0300

    Updating vscode server image

--- images/code-server/entrypoint.sh
+++ images/code-server/entrypoint.sh
@@ -2,4 +2,5 @@
 if [ -n "$USERNAME" ]; then
     sudo USERNAME=$USERNAME bash -c 'echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/nopasswd && usermod -l $USERNAME coder';
 fi
-sudo service ssh restart && dumb-init code-server --host 0.0.0.0 $@
+["dumb-init", "fixuid", "-q", "/usr/local/bin/code-server", "--host", "0.0.0.0", "."]
+sudo service ssh restart && dumb-init fixuid -q /usr/local/bin/code-server --host 0.0.0.0 . $@
